21b7e5f0996a822ea8813fa1c59bb7be7f30238c
ruchajoshi <joshirucha0807@gmail.com>
2019-11-25 21:22:33 +0000

    Add playactivity with design

--- app/src/main/java/com/ruchajoshi/bakingapplication/utilities/Constant.java
+++ app/src/main/java/com/ruchajoshi/bakingapplication/utilities/Constant.java
@@ -13,6 +13,11 @@ public final class Constant {
     public static final String[] TAP_TITLE = new String[] {"Ingredients", "Steps"};
     public static final int PAGE_COUNT = TAP_TITLE.length;
 
+    public static final String SAVE_STEP = "save_step";
+    public static final String STATE_STEP_INDEX = "state_step_index";
+    public static final String STATE_PLAYBACK_POSITION = "state_playback_position";
+    public static final String STATE_CURRENT_WINDOW = "state_current_window";
+    public static final String STATE_PLAY_WHEN_READY = "state_play_when_ready";
 
     public static final int GRID_COLUMN_WIDTH = 520;
     public static final int GRID_COLUMN_WIDTH_DEFAULT = 48;
